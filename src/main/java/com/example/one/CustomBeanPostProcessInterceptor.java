91aac07a165d824bc554210fab625dcc21989fe8
cp <cp@172.24.188.4>
2018-09-03 16:19:26 +0300

    PostProcessorInterceptor

---
+++ src/main/java/com/example/one/CustomBeanPostProcessInterceptor.java
@@ -0,0 +1,37 @@
+package com.example.one;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Interceptor which for each created bean wait until the random returns even
+ * value
+ * 
+ * @author cp
+ *
+ */
+@Component
+public class CustomBeanPostProcessInterceptor implements BeanPostProcessor {
+
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) {
+		if (bean instanceof com.example.one.Prototype) {
+			Prototype temp = ((com.example.one.Prototype) bean);
+			System.out.println("Current x: " + temp.x);
+		}
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (bean instanceof com.example.one.Prototype) {
+			Prototype temp = ((com.example.one.Prototype) bean);
+			while (temp.x % 2 != 0) {
+				temp.x = (int) (Math.random() * 10);
+			}
+		}
+		return bean;
+
+	}
+}
